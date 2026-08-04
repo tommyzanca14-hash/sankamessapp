7ecfaa195c0c11f08411bdbeb859b6caa826a2ce
tommyzanca14-hash <tommyzanca14@gmail.com>
2026-08-04 04:16:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
-const server = http.createServer(server ? server : app); // Compatibilità server
-const io = new Server(http.createServer(app) ? http.createServer(app) : server, {
+const server = http.createServer(app);
+const io = new Server(server, {
     cors: { origin: "*", methods: ["GET", "POST"] }
 });
 
-// File locale per salvare i dati in modo persistente su Render
 const DB_FILE = path.join(__dirname, 'database.json');
 
 function loadData() {
@@ -36,7 +35,6 @@ function saveData(data) {
 io.on('connection', (socket) => {
     console.log('Un utente si è connesso:', socket.id);
 
-    // Registrazione utente
     socket.on('register_user', (userData) => {
         let db = loadData();
         let existingUser = db.users.find(u => u.phone === userData.phone);
@@ -47,7 +45,6 @@ io.on('connection', (socket) => {
         socket.emit('registration_success', userData);
     });
 
-    // Aggiunta contatto
     socket.on('add_contact', (contactData) => {
         let db = loadData();
         db.contacts.push(contactData);
@@ -55,7 +52,6 @@ io.on('connection', (socket) => {
         io.emit('update_contacts', db.contacts);
     });
 
-    // Invio messaggio
     socket.on('send_message', (msgData) => {
         let db = loadData();
         db.messages.push(msgData);
@@ -63,16 +59,13 @@ io.on('connection', (socket) => {
         io.emit('receive_message', msgData);
     });
 
-    // Richiesta dati iniziali
     socket.on('get_initial_data', () => {
         let db = loadData();
         socket.emit('initial_data', { contacts: db.contacts, messages: db.messages });
     });
 });
 
-// Avvia il server sulla porta assegnata da Render o 3000
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server avviato sulla porta ${PORT}`);
 });
-     
