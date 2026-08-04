368dd6d81f57990ec5b142b6b2146f2e22a74bf4
tommyzanca14-hash <tommyzanca14@gmail.com>
2026-08-05 01:58:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 // Mappe per utenti online e messaggi offline in attesa
 const users = {};
